3395a62e7a00b3155e42fb62c49519a8fb72c9f4
NTsik0 <Tsikaridze.Nikoloz@kiu.edu.ge>
2024-12-26 23:47:22 +0400

    input var using console

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+class main {
+    // aige yvela saxeli  da mnishneloba
+    public static void GetAllValues(Variables[] a) {
+        for (int i = 0; i < a.length && a[i] != null; i++) {
+            System.out.print(a[i].GetName() + "  ");
+            System.out.println(a[i].GetData());
+
+        }
+
+    }
+
+    // amowmebs or ertnair saxelian cvlads
+    public static boolean Check(Variables[] a, String name) {
+        int o = 0;
+        for (int i = 0; i < a.length && a[i] != null; i++) {
+            a[i].equals(name);
+            o++;
+        }
+        if (o > 0) return false;
+        else return true;
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Variables[] Variables = new Variables[1000];
+        Scanner Scan1 = new Scanner(System.in);
+        System.out.println("package main");
+        System.out.println("import " + "\"fmt\"");
+        System.out.println("func main () {");
+        String SimpleLine = "";
+        int Amount = 0;
+        int index = 0;
+
+
+        while (true) {
+            String Line = "";
+            // TITO XAZi
+            Line = Line + Scan1.nextLine();
+
+            SimpleLine = Line.replaceAll("\\s+ ", " ");
+            SimpleLine = SimpleLine.trim();
+
+
+            // cvladis sheqmna
+            if (("" + Line.charAt(0) + Line.charAt(1) + Line.charAt(2)).equals("var")) {
+
+                int space = 0;
+                String Name = "";
+                String Value = "";
+                for (int i = 3; i < SimpleLine.length() && space != 2; i++) {
+
+                    if (("" + SimpleLine.charAt(i)).equals(" ")) {
+                        space++;
+                        continue;
+
+                    } else Name = Name + SimpleLine.charAt(i);
+
+                }
+                for (int k = 3; k < SimpleLine.length(); k++) {
+                    if (("" + SimpleLine.charAt(k)).equals("=")) {
+                        for (int j = k + 1; j < SimpleLine.length(); j++) {
+                            Value = Value + SimpleLine.charAt(j);
+
+                        }
+
+                    }
+
+                }
+                if (Check(Variables, Name)) {
+
+                    Variables[index] = new Variables(Name, Integer.parseInt(Value.trim()));
+                    index++;
+
+                }
+            }
+        }
     }
 }
