86766712a5d4fed4c9235e01f07698ae3bc442fa
NTsik0 <Tsikaridze.Nikoloz@kiu.edu.ge>
2024-12-30 23:57:45 +0400

    Main - For Loop Update v5.0

--- src/main.java
+++ src/main.java
@@ -49,6 +49,106 @@ class main {
         }
     }
 
+
+    //foris methodi
+    public static void ForMethod(String[] LinesArray, int length, Variables[] Variables, int index) {
+        String Shemotanili="";
+        int xyz=0;
+
+        for (int i = 0; i < length; i++) {
+            System.out.println("mamamamdasmda");
+        }
+        for (int ku = 0; ku < length && LinesArray[ku] != null; ku++) {
+            System.out.println("shedis?");
+            String JustLine = LinesArray[ku];
+
+            // vamowmebt tu sout-is inputi iwyeba sworad
+            if (JustLine.length() > 2) {
+                String Value1 = "";
+                String Value2 = "";
+                String Operation;
+                while (!(LinesArray[ku].equals("break"))) {
+                    if (!LinesArray[ku].equals("}")) System.out.println(LinesArray[ku]);
+
+
+
+                    for (int i = 1; i < JustLine.length(); i++) {
+                        String NameForUpdate = "";
+                        if (JustLine.charAt(i - 1) == '+' && JustLine.charAt(i) == '+') {
+                            for (int j = 0; j < i - 1; j++) {
+                                NameForUpdate = NameForUpdate + JustLine.charAt(j);
+
+
+                            }
+                            if (CheckIfExists(Variables, NameForUpdate)) {
+                                int hgk = Integer.parseInt(SearchByName(Variables, NameForUpdate));
+                                hgk++;
+                                Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(hgk);
+
+                            }
+
+                        }
+                    }
+
+
+
+
+
+                    if (JustLine.length() >= 3) {
+
+                        if (!("" + JustLine.charAt(0) + JustLine.charAt(1) + JustLine.charAt(2)).equals("var")) {
+                            for (int i = 1; i < JustLine.length(); i++) {
+                                String NameForUpdate = "";
+
+                                if (("" + JustLine.charAt(i)).equals("=") && !("" + JustLine.charAt(i - 1)).equals(":")) {
+                                    int operationIndex = -1;
+                                    for (int h = i + 1; h < JustLine.length(); h++) {
+                                        if (("" + JustLine.charAt(h)).equals("+") || ("" + JustLine.charAt(h)).equals("-") || ("" + JustLine.charAt(h)).equals("/") || ("" + JustLine.charAt(h)).equals("*") || ("" + JustLine.charAt(h)).equals("%")) {
+                                            operationIndex = h;
+                                            break;
+                                        }
+                                    }
+
+                                    if (operationIndex != -1) {
+                                        Operation = "" + JustLine.charAt(operationIndex);
+                                        for (int j = 0; j < i; j++) {
+                                            NameForUpdate += JustLine.charAt(j);
+                                        }
+
+                                        if (CheckIfExists(Variables, NameForUpdate)) {
+                                            for (int k = i + 1; k < operationIndex; k++) {
+                                                Value1 += JustLine.charAt(k);
+                                            }
+                                            for (int k = operationIndex + 1; k < JustLine.length(); k++) {
+                                                Value2 += JustLine.charAt(k);
+                                            }
+
+                                            int result = Integer.parseInt(CalculatorSimple.Do(Variables, Value1.trim(), Operation, Value2.trim()));
+                                            Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(result);
+
+                                        } else {
+                                            System.out.println("NOT CORRECT INPUT");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (ku < length - 1) ku++;
+                    else ku = 0;
+
+
+
+                }
+
+            }
+
+        }
+
+        System.out.println("Check If Enters");
+    }
+
+
     public static void main(String[] args) {
         Variables[] Variables = new Variables[1000];
         Scanner Scan1 = new Scanner(System.in);
@@ -56,16 +156,19 @@ class main {
         System.out.println("import \"fmt\"");
         System.out.println("func main () {");
         String SimpleLine = "";
-        int Amount = 0;
+        int ForExists = 0;
+        int ForExists1 = 0;
         int index = 0;  // es gamoiyeneba Variable klasis indeqsistvis
         int Count = 1;
         String Shemotanili = "";
         Queue<String> Valuablebisqueue = new LinkedList<>();
         int xyz = 0;
+        String[] Lines = new String[1002];
+        int IndexL = 0;
+        boolean ForBreak = false;
 
         boolean Break = false;
         while (true) {
-            System.out.println(Count);
 
 
             int NumberOfBr = 1;
@@ -82,96 +185,10 @@ class main {
                 if (("" + JustLine.charAt(i)).equals("{")) Count++;
                 if (("" + JustLine.charAt(i)).equals("}")) Count--;
             }
-//===========================================================================================================================================
-            boolean ChawerisSignali = false;
-            String ChasaweriIfshi = "";
-            String Frchxilebshi = "";
+            // while
+
+
             if (!JustLine.equals("") && !SimpleLine.equals("")) {
-                if (JustLine.startsWith("if")) {
-                    for (int i = 1; i < JustLine.length(); i++) {
-                        if ((("" + JustLine.charAt(i)).equals("("))) {
-                            for (int j = i+1; j < JustLine.length() && (JustLine.charAt(j)!=')'); j++) {
-                                Frchxilebshi = Frchxilebshi + JustLine.charAt(j);
-                            }
-                        }
-
-                    }
-
-                }
-
-            }
-            System.out.println(Frchxilebshi);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//=====================================================================================================================================================
-            if (JustLine.equals("break")){
-                Break=true;
-//                System.out.println(Break);
-//                System.out.println(1);
-            }
-            System.out.println(Break);
-            if (!JustLine.equals("") && !SimpleLine.equals("")) {
-                if (JustLine.startsWith("fortrue")) {
-                    continue;
-                }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 // amowmebs var aris tuara stringshi,shemoaqvs var it sheqmnili cvladebi  mag : var name int =1
@@ -189,7 +206,7 @@ class main {
                             }
                         }
                         // SimpleLine-is gamoyenebit rac weria tolobis marjvniv vigebt da vwert values mnishvnelobas
-                        for (int k = 3; k < SimpleLine.length(); ++k) {
+                        for (int k = 3; k < SimpleLine.length(); k++) {
                             if (("" + SimpleLine.charAt(k)).equals("=")) {
                                 for (int j = k + 1; j < SimpleLine.length(); ++j) {
                                     Value = Value + SimpleLine.charAt(j);
@@ -226,80 +243,76 @@ class main {
                         }
                     }
                 }
-
-                String Value1 = "";
-                String Value2 = "";
-                String Operation;
-                // gagas gatolebis da informaciis shecvlis
-                // kalkulaturi
-                // --------------------------------------------------------------------------------------------------------------------------------------------------------------========================================================
-                if (JustLine.length() > 2) {
-                    String NameForUpdate = "";
+                if (ForExists != 0) {
+                    String Value1 = "";
+                    String Value2 = "";
+                    String Operation;
+                    // gatolebis da informaciis shecvlis
+                    // kalkulaturi
+                    // --------------------------------------------------------------------------------------------------------------------------------------------------------------========================================================
+                    if (JustLine.length() > 2) {
+                        String NameForUpdate = "";
 //                    xdggf++
-                    for (int i = 1; i < JustLine.length(); i++) {
-                        if (JustLine.charAt(i - 1) == '+' && JustLine.charAt(i) == '+') {
-                            for (int j = 0; j < i - 1; j++) {
-                                NameForUpdate = NameForUpdate + JustLine.charAt(j);
+                        for (int i = 1; i < JustLine.length(); i++) {
+                            if (JustLine.charAt(i - 1) == '+' && JustLine.charAt(i) == '+') {
+                                for (int j = 0; j < i - 1; j++) {
+                                    NameForUpdate = NameForUpdate + JustLine.charAt(j);
 
+
+                                }
+                                if (CheckIfExists(Variables, NameForUpdate)) {
+                                    int hgk = Integer.parseInt(SearchByName(Variables, NameForUpdate));
+                                    hgk++;
+                                    Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(hgk);
+
+                                }
 
                             }
-                            if (CheckIfExists(Variables, NameForUpdate)) {
-                                int hgk = Integer.parseInt(SearchByName(Variables, NameForUpdate));
-                                hgk++;
-                                Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(hgk);
-
-                            }
-
                         }
+
+
+
                     }
-
-//                     gazarde
-
-
-                }
 
 //=====================================================================================================================================================================================================================
 
 
-                /*
 
-                 */
+                    if (SimpleLine.length() >= 3) {
 
+                        if (!("" + JustLine.charAt(0) + JustLine.charAt(1) + JustLine.charAt(2)).equals("var")) {
+                            for (int i = 1; i < JustLine.length(); i++) {
+                                String NameForUpdate = "";
 
-                if (SimpleLine.length() >= 3) {
-
-                    if (!("" + JustLine.charAt(0) + JustLine.charAt(1) + JustLine.charAt(2)).equals("var")) {
-                        for (int i = 1; i < JustLine.length(); i++) {
-                            String NameForUpdate = "";
-
-                            if (("" + JustLine.charAt(i)).equals("=") && !("" + JustLine.charAt(i - 1)).equals(":")) {
-                                int operationIndex = -1;
-                                for (int h = i + 1; h < JustLine.length(); h++) {
-                                    if (("" + JustLine.charAt(h)).equals("+") || ("" + JustLine.charAt(h)).equals("-") || ("" + JustLine.charAt(h)).equals("/") || ("" + JustLine.charAt(h)).equals("*") || ("" + JustLine.charAt(h)).equals("%")) {
-                                        operationIndex = h;
-                                        break;
-                                    }
-                                }
-
-                                if (operationIndex != -1) {
-                                    Operation = "" + JustLine.charAt(operationIndex);
-                                    for (int j = 0; j < i; j++) {
-                                        NameForUpdate += JustLine.charAt(j);
+                                if (("" + JustLine.charAt(i)).equals("=") && !("" + JustLine.charAt(i - 1)).equals(":")) {
+                                    int operationIndex = -1;
+                                    for (int h = i + 1; h < JustLine.length(); h++) {
+                                        if (("" + JustLine.charAt(h)).equals("+") || ("" + JustLine.charAt(h)).equals("-") || ("" + JustLine.charAt(h)).equals("/") || ("" + JustLine.charAt(h)).equals("*") || ("" + JustLine.charAt(h)).equals("%")) {
+                                            operationIndex = h;
+                                            break;
+                                        }
                                     }
 
-                                    if (CheckIfExists(Variables, NameForUpdate)) {
-                                        for (int k = i + 1; k < operationIndex; k++) {
-                                            Value1 += JustLine.charAt(k);
-                                        }
-                                        for (int k = operationIndex + 1; k < JustLine.length(); k++) {
-                                            Value2 += JustLine.charAt(k);
+                                    if (operationIndex != -1) {
+                                        Operation = "" + JustLine.charAt(operationIndex);
+                                        for (int j = 0; j < i; j++) {
+                                            NameForUpdate += JustLine.charAt(j);
                                         }
 
-                                        int result = Integer.parseInt(CalculatorSimple.Do(Variables, Value1.trim(), Operation, Value2.trim()));
-                                        Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(result);
+                                        if (CheckIfExists(Variables, NameForUpdate)) {
+                                            for (int k = i + 1; k < operationIndex; k++) {
+                                                Value1 += JustLine.charAt(k);
+                                            }
+                                            for (int k = operationIndex + 1; k < JustLine.length(); k++) {
+                                                Value2 += JustLine.charAt(k);
+                                            }
 
-                                    } else {
-                                        System.out.println("NOT CORRECT INPUT");
+                                            int result = Integer.parseInt(CalculatorSimple.Do(Variables, Value1.trim(), Operation, Value2.trim()));
+                                            Variables[Integer.parseInt(SearchByNameIndex(Variables, NameForUpdate))].SetData(result);
+
+                                        } else {
+                                            System.out.println("NOT CORRECT INPUT");
+                                        }
                                     }
                                 }
                             }
@@ -307,6 +320,28 @@ class main {
                     }
                 }
 //==========================================================================================================================================================================================================
+                // fori ,lainebs inaxav Linesshi  fori bolo brchxilianad
+                if (JustLine.startsWith("fortrue")) {
+                    ForExists++;
+                    ForExists1++;
+                }
+                if (JustLine.equals("break")) {
+
+                    ForBreak = true;
+                }
+
+                System.out.println(ForExists);
+                if (!(JustLine.equals("fortrue{")) && ForExists > 0) {
+                    Lines[IndexL] = JustLine;
+                    System.out.println(IndexL + " ->  " + Lines[IndexL]);
+                    IndexL++;
+
+                }
+                if (Count == 1 && ForExists > 0) ForExists--;
+                if (Count == 1 && ForExists == 0) continue;
+//                indexl raodenoba
+//                for true shi
+//                lines [indexl] == break
 
 
 //==========================================================================================================================================================================================================
@@ -339,9 +374,30 @@ class main {
             // ranavs im shemtxvevashi tu bolo } daxuruli
 
 
-            if (Count == 0 && Break) {
+            if (ForExists1 > 0) {
+                if (Count == 0 && !Break) {
+                    String[] ForMethodArray = new String[IndexL];
+                    for (int i = 0; i < Lines.length && Lines[i] != null; i++) {
+                        ForMethodArray[i] = Lines[i];
+                    }
+//                for (int i = 0; i <IndexL; i++) {
+//                    System.out.println(i + "-------------->>>>>>>>>>" + ForMethodArray[i]);
+//                }
 
 
+                    ForMethod(ForMethodArray, IndexL, Variables, index);
+
+
+                    while (!Valuablebisqueue.isEmpty()) {
+                        String cvladisSaxeli = Valuablebisqueue.poll();//queues pirvel elements gvichvenebs da shlis
+                        System.out.println(SearchByName(Variables, cvladisSaxeli).replaceAll("\"", ""));
+
+                        xyz++;
+// xyz ++
+                    }
+                    break;
+                }
+            } else if (Count == 0) {
                 while (!Valuablebisqueue.isEmpty()) {
                     String cvladisSaxeli = Valuablebisqueue.poll();//queues pirvel elements gvichvenebs da shlis
                     System.out.println(SearchByName(Variables, cvladisSaxeli).replaceAll("\"", ""));
@@ -351,8 +407,8 @@ class main {
                 }
                 break;
             }
-
-
         }
+
+
     }
 }
